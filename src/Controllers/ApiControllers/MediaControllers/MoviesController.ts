7e8e4c8580e7c325af2a576e8c2947dc15568e0e
Pedro M. Silva <pemiolsi@hotmail.com>
2021-07-06 11:57:00 +0100

    Allow filtering movies by quality fields

--- src/Controllers/ApiControllers/MediaControllers/MoviesController.ts
+++ src/Controllers/ApiControllers/MediaControllers/MoviesController.ts
@@ -4,6 +4,7 @@ import { Request, Response } from "restify";
 import * as r from 'rethinkdb';
 import { MediaTableController } from "./MediaController";
 import { Route } from "../../BaseController";
+import { InvalidArgumentError } from 'restify-errors';
 
 export class MoviesController extends MediaTableController<MovieMediaRecord> {
     sortingFields: string[] = [ 
@@ -16,13 +17,44 @@ export class MoviesController extends MediaTableController<MovieMediaRecord> {
     }
 
     getQuery ( req : Request, res : Response, query : r.Sequence ) : r.Sequence {
-        return this.getTransientQuery( req, 
-                this.getCollectionsQuery( req,
-                this.getGenresQuery( req, 
-                this.getRepositoryPathsQuery( req,
-                this.getWatchedQuery( req,
-                    super.getQuery( req, res, query )
-                ) ) ) ) );
+        query = super.getQuery( req, res, query );
+        query = this.getWatchedQuery( req, query );
+        query = this.getRepositoryPathsQuery( req, query );
+        query = this.getGenresQuery( req, query );
+        query = this.getCollectionsQuery( req, query );
+        query = this.getQualityQuery( req, query );
+        query = this.getTransientQuery( req, query );
+
+        return query;
+    }
+
+    protected getQualityFieldQuery ( query : r.Sequence, field: string, filters?: Record<string, string> ) : r.Sequence {
+        if ( typeof filters === 'object' ) {
+            const keys = Object.keys( filters );
+
+            const included = keys.filter( key => filters[ key ] === 'include' );
+            const excluded = keys.filter( key => filters[ key ] === 'exclude' );
+
+            if ( included.length > 0 ) {
+                query = query.filter( ( doc ) => {
+                    return r.expr( included ).contains( doc( "quality" )( field ) as any );
+                } )
+            } else if ( excluded.length > 0 ) {
+                query = query.filter( ( doc ) => {
+                    return r.expr( excluded ).contains( doc( "quality" )( field ) as any ).not();
+                } );
+            }
+        }
+
+        return query;
+    }
+
+    public getQualityQuery ( req : Request, query : r.Sequence ) : r.Sequence {
+        query = this.getQualityFieldQuery( query, 'resolution', req.query.filterResolutions );
+        query = this.getQualityFieldQuery( query, 'source', req.query.filterSources );
+        query = this.getQualityFieldQuery( query, 'colorGamut', req.query.filterColorGamuts );
+
+        return query;
     }
 
     async transformAll ( req : Request, res : Response, items : MovieMediaRecord[] ) : Promise<any[]> {
@@ -52,5 +84,24 @@ export class MoviesController extends MediaTableController<MovieMediaRecord> {
         return this.table.find( query => {
             return ( query as any ).distinct( { index: 'genres' } );
         } );
+    }
+
+    @Route( 'get', '/quality/:field' )
+    async qualities ( req : Request, res : Response ) {
+        if ( req.params.field === 'resolutions' ) {
+            return this.table.find( query => {
+                return ( query as any ).distinct( { index: 'qualityResolutions' } );
+            } );
+        } else if ( req.params.field === 'sources' ) {
+            return this.table.find( query => {
+                return ( query as any ).distinct( { index: 'qualitySources' } );
+            } )
+        } else if ( req.params.field === 'color-gamuts' ) {
+            return this.table.find( query => {
+                return ( query as any ).distinct( { index: 'qualityColorGamuts' } );
+            } )
+        } else {
+            throw new InvalidArgumentError( `The quality field ${ req.params.field } is invalid, expected one of: resolutions, sources, color-gamuts.` );
+        }
     }
 }
