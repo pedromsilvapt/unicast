5994b533020792d6738c3a173579292222118779
Pedro M. Silva <pemiolsi@hotmail.com>
2024-09-02 15:53:03 +0100

    feat: re-calculate internalId when translating local paths

--- src/Tools/Migrations/TranslatePaths.ts
+++ src/Tools/Migrations/TranslatePaths.ts
@@ -1,9 +1,10 @@
 import { Tool, ToolOption, ToolValueType } from "../Tool";
-import { isPlayableRecord, MediaRecord } from '../../MediaRecord';
+import { isMovieRecord, isPlayableRecord, isTvEpisodeRecord, MediaRecord } from '../../MediaRecord';
 import { AbstractMediaTable } from '../../Database/Database';
 import { FileSystemRepository } from '../../Extensions/MediaRepositories/FileSystem/FileSystemRepository';
 import { AsyncBreaker } from '../../ES2017/AsyncBreaker';
 import * as path from 'path';
+import { pp } from 'clui-logger';
 
 export enum PathStyle {
     Win32 = 'win32',
@@ -15,6 +16,8 @@ export interface TranslatePathsOptions {
     dryRun: boolean;
     listSeparator: string;
     pairSeparator: string;
+    sourcePlatform: PathStyle;
+    targetPlatform: PathStyle;
 }
 
 export class TranslatePathsTool extends Tool<TranslatePathsOptions> {
@@ -23,6 +26,8 @@ export class TranslatePathsTool extends Tool<TranslatePathsOptions> {
             new ToolOption( 'dryRun' ).setRequired( false ).setType( ToolValueType.Boolean ).setDefaultValue( false ),
             new ToolOption( 'listSeparator' ).setRequired( false ).setType( ToolValueType.String ).setDefaultValue( ';' ),
             new ToolOption( 'pairSeparator' ).setRequired( false ).setType( ToolValueType.String ).setDefaultValue( '=' ),
+            new ToolOption( 'sourcePlatform' ).setRequired( false ).setType( ToolValueType.String ).setDefaultValue( PathStyle.Win32 ),
+            new ToolOption( 'targetPlatform' ).setRequired( false ).setType( ToolValueType.String ).setDefaultValue( PathStyle.Posix ),
         ]
     }
 
@@ -58,7 +63,18 @@ export class TranslatePathsTool extends Tool<TranslatePathsOptions> {
             .map( pair => ( { original: pair[ 0 ], destination: pair[ 1 ] } ) );
 
         const breaker = new AsyncBreaker();
-            
+        
+        if (options.sourcePlatform != PathStyle.Win32 && options.sourcePlatform != PathStyle.Posix) {
+            this.logger.error(pp`Invalid value ${options.sourcePlatform} for arg ${"sourcePlatform"}, valid options are: ${PathStyle.Win32} and ${PathStyle.Posix}`);
+        }
+        
+        if (options.targetPlatform != PathStyle.Win32 && options.targetPlatform != PathStyle.Posix) {
+            this.logger.error(pp`Invalid value ${options.targetPlatform} for arg ${"targetPlatform"}, valid options are: ${PathStyle.Win32} and ${PathStyle.Posix}`);
+        }
+        
+        const sourceSep = path[options.sourcePlatform].sep;
+        const targetSep = path[options.targetPlatform].sep;
+        
         for ( let table of tables ) { 
             await table.connection.transaction(async trx => {
                 for ( let record of await table.findStream( null, { transaction: trx } ).toArray() ) {
@@ -99,9 +115,13 @@ export class TranslatePathsTool extends Tool<TranslatePathsOptions> {
                                     continue;
                                 }
                                 
-                                newPath = path.join( transform.destination, newPath.slice( transform.original.length ) )
-                                    .split( path.win32.sep )
-                                    .join( path.posix.sep );
+                                newPath = path.join( transform.destination, newPath.slice( transform.original.length ) );
+                                
+                                if ( sourceSep != targetSep ) {
+                                    newPath = newPath
+                                        .split( sourceSep )
+                                        .join( targetSep );
+                                }
                                 
                                 if ( newPath != oldPath ) {
                                     source.id = newPath;
@@ -111,12 +131,20 @@ export class TranslatePathsTool extends Tool<TranslatePathsOptions> {
                             }
                             
                             if ( changed ) {
+                                const newPath = record.sources[ 0 ].id;
+
+                                if ( isTvEpisodeRecord( record ) || isMovieRecord( record ) ) {
+                                    record.internalId = this.server.hash( newPath );
+                                }
+                            }
+                            
+                            if ( changed ) {
                                 recordsChanged += 1;
             
                                 this.log( record.kind, record.title, JSON.stringify( record.sources ) );
             
                                 if ( !options.dryRun ) {
-                                    await table.update( record.id, { sources: record.sources }, { transaction: trx } );
+                                    await table.update( record.id, { sources: record.sources, internalId: record.internalId }, { transaction: trx } );
                                 }
                             }
                         }
